07994896a8e71ef247fd41b769a106a85a9db67a
achul <waduhgaming554@gmail.com>
2025-03-12 15:09:12 +0700

    route bagian a

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID ' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
